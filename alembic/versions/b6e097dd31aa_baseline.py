243f91ad279a79fde538b404faef98de566fc544
Gigi Sayfan <gigi.sayfan@helix.com>
2017-07-09 11:29:22 -0700

    Add baseline and index migrations

---
+++ alembic/versions/b6e097dd31aa_baseline.py
@@ -0,0 +1,24 @@
+"""baseline
+
+Revision ID: b6e097dd31aa
+Revises: 
+Create Date: 2017-07-09 10:59:01.382315
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b6e097dd31aa'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
