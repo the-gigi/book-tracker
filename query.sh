1281b5be4262002fedacd9e8eb46b6dd662003ef
Gigi <the.gigi@gmail.com>
2020-05-13 12:47:38 -0700

    added query.sh for quintessential query

---
+++ query.sh
@@ -0,0 +1,1 @@
+sqlite3 book-tracker.db "select book_id, strftime('%m-%d-%Y %H:%M', timestamp), rank, change from rank where timestamp in (select timestamp from rank where change < -30000) and category_id=(select id from category where name='Amazon Best Sellers Rank') and change < -30000 and timestamp > datetime('now','-2 day');"
