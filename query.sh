256c71371a6283d0d63e5d0160ca9a26b98bb18b
the-gigi <the.gigi@gmail.com>
2020-08-31 04:05:53 -0700

    properly fix query

--- query.sh
+++ query.sh
@@ -3,4 +3,5 @@ if [[ $days == "" ]]; then
   days=2
 fi
 
-sqlite3 book-tracker.db "select book_id, strftime('%m-%d-%Y %H:%M', timestamp), rank, change from rank where timestamp in (select timestamp from rank where change < -30000) and category_id=(select id from category where name='Amazon Best Sellers Rank') and change < -10000 and timestamp > datetime('now','-${days} day');"
+min_change=10000
+sqlite3 book-tracker.db "select book_id, strftime('%m-%d-%Y %H:%M', timestamp), rank, change from rank where timestamp in (select timestamp from rank where change < -${min_change}) and category_id=(select id from category where name='Amazon Best Sellers Rank') and change < -${min_change} and timestamp > datetime('now','-${days} day');"
