317e597c1bb09021351288397de7ff071413240e
the-gigi <the.gigi@gmail.com>
2020-08-31 04:01:10 -0700

    good times. take into account changes of 10,000 and up

--- query.sh
+++ query.sh
@@ -3,4 +3,4 @@ if [[ $days == "" ]]; then
   days=2
 fi
 
-sqlite3 book-tracker.db "select book_id, strftime('%m-%d-%Y %H:%M', timestamp), rank, change from rank where timestamp in (select timestamp from rank where change < -30000) and category_id=(select id from category where name='Amazon Best Sellers Rank') and change < -30000 and timestamp > datetime('now','-${days} day');"
+sqlite3 book-tracker.db "select book_id, strftime('%m-%d-%Y %H:%M', timestamp), rank, change from rank where timestamp in (select timestamp from rank where change < -30000) and category_id=(select id from category where name='Amazon Best Sellers Rank') and change < -10000 and timestamp > datetime('now','-${days} day');"
