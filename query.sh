bdbeedb6f2e4a54272daca0b3ceda290ba9ed94f
the-gigi <the.gigi@gmail.com>
2020-09-08 17:58:28 -0700

    min change 12000

--- query.sh
+++ query.sh
@@ -3,5 +3,5 @@ if [[ $days == "" ]]; then
   days=2
 fi
 
-min_change=10000
+min_change=12000
 sqlite3 book-tracker.db "select book_id, strftime('%m-%d-%Y %H:%M', timestamp), rank, change from rank where timestamp in (select timestamp from rank where change < -${min_change}) and category_id=(select id from category where name='Amazon Best Sellers Rank') and change < -${min_change} and timestamp > datetime('now','-${days} day');"
