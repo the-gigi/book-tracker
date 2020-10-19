7fc26acbe6d3e5d165a4c4ea0a463b34e4d41c56
the-gigi <the.gigi@gmail.com>
2020-10-19 09:25:27 -0700

    support environment variable for minimum change (default 10,000). diplay timestamp first

--- query.sh
+++ query.sh
@@ -3,7 +3,7 @@ if [[ $days == "" ]]; then
   days=2
 fi
 
-min_change=15000
-echo min_change:  "$min_change"
+# minimum change in rank to consider (default to 10,000 if environment variable is not defined)
+min_change="${BOOK_TRACKER_MIN_CHANGE-10000}"
 
-sqlite3 book-tracker.db "select book_id, strftime('%m-%d-%Y %H:%M', timestamp), rank, change from rank where timestamp in (select timestamp from rank where change < -${min_change}) and category_id=(select id from category where name='Amazon Best Sellers Rank') and change < -${min_change} and timestamp > datetime('now','-${days} day');"
+sqlite3 book-tracker.db "select strftime('%m-%d-%Y %H:%M', timestamp), book_id, rank, change from rank where timestamp in (select timestamp from rank where change < -${min_change}) and category_id=(select id from category where name='Amazon Best Sellers Rank') and change < -${min_change} and timestamp > datetime('now','-${days} day');"
